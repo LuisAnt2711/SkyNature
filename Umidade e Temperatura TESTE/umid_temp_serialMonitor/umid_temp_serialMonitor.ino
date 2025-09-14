6ed71d55f2e4d74baf599809fdc9c0b58d9834c2
LuisAnt2711 <luisantonio272009@gmail.com>
2025-09-14 17:36:34 -0300

    Update Umid/Temp

--- Umidade e Temperatura TESTE/umid_temp_serialMonitor/umid_temp_serialMonitor.ino
+++ Umidade e Temperatura TESTE/umid_temp_serialMonitor/umid_temp_serialMonitor.ino
@@ -1,5 +1,6 @@
-const int trimpot = A7;
-const int lm35pin = A0;
+// Dá nome ás entradas
+const int trimpot = A7;   //Umidade (Trimpot)
+const int lm35pin = A0;   //Temperatura (LM35)
 
 void setup() {
 
@@ -9,17 +10,24 @@ void setup() {
 
 void loop() {
 
-  int valorA0 = analogRead(lm35pin);
-  int valorA7 = analogRead(trimpot);
-  float tempC = (valorA0 * 5.0 * 100.0) / 1023.0;
-  float umidS = (valorA7 * 100.0) / 1023.0;
+// Atribui uma variável para cada entrada
+  int valorA0 = analogRead(lm35pin);   //Temperatura
+  int valorA7 = analogRead(trimpot);   //Umidade
 
+//Caucula o valor de cada um dos sensores
+  float tempC = (valorA0 * 5.0 * 100.0) / 1023.0;   //Temperatura
+  float umidS = (valorA7 * 100.0) / 1023.0;         //Umidade
+
+//Escreve os valores no Serial Monitor a cada 1 segundo
   Serial.print("Umidade: ");
   Serial.print(umidS, 1);
   Serial.println("%");
+
   Serial.print("Temperatura: ");
-  Serial.print(tempC);
-  Serial.print((char)223);
+  Serial.print(tempC, 1);
+  Serial.print("°"); 
   Serial.println("C");
+
   delay(1000);
+
 }
